331b1d3e12b1cdd8476553f91d84e5518e7e99e6
wanjikuciku <lorna.wanjiku56@gmail.com>
2018-12-17 09:24:14 +0300

    changed links for script.js to the head section in index.html

--- js/script.js
+++ js/script.js
@@ -1,11 +1,50 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+var player1;
+var player2;
+var currentPlayer;
 function rollDice() {
-  var die1 = document.getElementById("die1");
-  var die2 = document.getElementById("die2");
-  var status = document.getElementById("status");
-  var d1 = Math.floor(Math.random() * 6) + 1;
-  var d2 = Math.floor(Math.random() * 6) + 1;
-  var diceTotal = d1 + d2;
-  die1.innerHTML = d1;
-  die2.innerHTML = d2;
-  status.innerHTML = "you rolled "+diceTotal+".";
+   return Math.floor(Math.random() * 6) + 1;
+   }
+
+function Player(turn){
+  this.roll = 0;
+  this.mainscore = 0;
+  this.score = 0;
+  this.turn = turn;
+  this.playerName;
 }
+
+
+
+
+
+
+// User Interface
+$(document).ready(function() {
+  $("button#begin").submit(function(event){
+    event.preventDefault();
+    player1 = new Player(true);
+    player2 = new Player(false);
+    $(".pig-dice").show();
+    $(".start-game").hide();
+  });
+
+    var player1Name = $(".player1Name").val();
+    $("#player1Name").text(player1Name);
+
+    var player2Name = $(".player2Name").val();
+    $("#player2Name").text(player2Name);
+
+    player1.playerName=player1Name;
+    player2.playerName=player2Name;
+
+  });
